d91d8c2cf8f0ef752b1c3ecca9ec79dc729cb243
TundePappova <TundePappova@TndePC>
2019-11-22 09:37:18 +0100

    Tunde to World

--- src/main/java/tunde/pappova/prezencka/App.java
+++ src/main/java/tunde/pappova/prezencka/App.java
@@ -6,6 +6,6 @@ package tunde.pappova.prezencka;
  */
 public class App {
 	public static void main(String[] args) {
-		System.out.println("Hello Tunde!");
+		System.out.println("Hello World!");
 	}
 }
